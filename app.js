8e609d6a8381abb13a7d6342b27a3be9d255a210
VarunnSharma <officialvarun240@gmail.com>
2026-04-28 07:13:01 +0530

    added health

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ app.use("/api/auth", require("./src/modules/auth/auth.routes"));
 app.use("/api/content", require("./src/modules/content/content.routes"));
 app.use("/api/approval", require("./src/modules/approval/approval.routes"));
 app.use("/api/public", require("./src/modules/public/public.routes"));
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server is healthy" });
+});
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is ready" });
+});
 
 sequelize.sync().then(() => {
     app.listen(process.env.PORT, () =>
-        console.log("Server running on port " + process.env.PORT)
+        console.log("SQL Server running ")
     );
 });
